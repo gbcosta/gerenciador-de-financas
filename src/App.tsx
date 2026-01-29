4db97f16dbf6ae02d818ac40fd94cba1c4faa13b
gbcosta <gbcostasant@gmail.com>
2026-01-29 14:57:46 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className=" flex justify-center bg-[#030712]">
+      <div className="bg-[#030712] min-h-screen w-3xl">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="mt-8">
+      <div>
+        <h1 className="text-white text-2xl font-bold">Nova Finance</h1>
+        <span className="text-zinc-300">
+          Good morning! Your finances are looking solid.
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
